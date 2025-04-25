f72414616353418b332c2e256cac7edd78d5b32a
lamseey <Salim.QADDA@um6p.ma>
2025-04-25 18:52:33 +0100

    router fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
+import Dashboard from '../views/DashboardView.vue'
+import Login from '../views/LoginView.vue'
+import Register from '../views/RegisterView.vue'
+import Objectives from '../views/ObjectivesView.vue'
+import Profile from '../views/ProfileView.vue'
+import Projects from '../views/ProjectsView.vue'
+import SkillTracker from '../views/SkillTrackerView.vue'
+import Timeline from '../views/TimelineView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Dashboard
   },
   {
     path: '/login',
@@ -19,6 +24,31 @@ const routes = [
     name: 'register',
     component: Register,
   },
+  {
+    path: "/objectives/:objectiveId",
+    name: "objective",
+    component: Objectives,
+  },
+  {
+    path: "/profile/:profileId",
+    name: "profile",
+    component: Profile,
+  },
+  {
+    path: "/projects/:userId",
+    name: "projects",
+    component: Projects,
+  },
+  {
+    path: "/skill-tracker/:userId",
+    name: "skill-tracker",
+    component: SkillTracker,
+  },
+  {
+    path: "/timeline/:userId",
+    name: "timeline",
+    component: Timeline,
+  }
 ]
 
 const router = createRouter({
