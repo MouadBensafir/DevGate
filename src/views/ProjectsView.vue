a268eb9f37a278e17145ba6621588da23564bd02
lamseey <Salim.QADDA@um6p.ma>
2025-04-26 22:49:13 +0100

    projects perfect

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -7,12 +7,20 @@ const actualUserId = route.params.userId;
 </script>
 
 <template>
-  <router-link :to="'/users/' + actualUserId + '/projects/new'">
-CREATE
-    </router-link>
-  <ProjectList :user-id="actualUserId"/>
+  <div class="container mt-4">
+    <div class="d-flex justify-content-between align-items-center mb-4">
+      <h1 class="mb-0">My Projects</h1>
+      <router-link
+        :to="'/users/' + actualUserId + '/projects/new'"
+        class="btn btn-primary"
+      >
+        <i class="bi bi-plus-lg me-2"></i>CREATE PROJECT
+      </router-link>
+    </div>
+    <ProjectList :user-id="actualUserId"/>
+  </div>
 </template>
 
 <style scoped>
-
+/* Optional custom styles can go here */
 </style>
